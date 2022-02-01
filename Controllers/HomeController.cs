8207ff4a41fb068ee34e891eaa951d4af05e9322
ASR0RB3K <asrorbek.design@gmail.com>
2022-02-01 22:03:54 +0500

    Admin Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,20 +16,34 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger, RowDbContext context)
     {
         _logger = logger;
-        _dbcontext=context;
+        _dbcontext = context;
     }
 
     public IActionResult Index()
     {
-        var rows = _dbcontext.rows.ToList();
+        var rows = _dbcontext.rows.OrderBy(p=>p.CreatedAt).ToList();
         return View(rows);
     }
 
-    // public IActionResult AdminPage()
-    // {
-    //     var queues=_dbcontext.rows;
-    //     return View(queues);
-    // }
+    [HttpGet]
+    public IActionResult Admin()
+    {
+        var rows = _dbcontext.rows.OrderBy(p => p.CreatedAt).ToList();
+        return View(rows);
+    }
+
+    [HttpPost]
+    public IActionResult Admin([FromForm]RowViewModel obj)
+    {
+        if (!_dbcontext.rows.Any(t => t.Id == obj.Id))
+        {
+            return NotFound();
+        }
+        _dbcontext.rows.Remove(obj);
+        _dbcontext.SaveChanges();
+        TempData["success"] = " Mijozga hizmat ko'rsatildi.!!";
+        return RedirectToAction("Adminpage");
+    }
 
     [HttpGet]
     public IActionResult TakeRow()
@@ -37,26 +51,51 @@ public class HomeController : Controller
         return View();
     }
     [HttpPost]
-    public IActionResult TakeRow([FromForm] RowViewModel model)
+    public IActionResult TakeRow([FromForm]RowViewModel model)
     {
-        var user = new RowViewModel();
+        var first = _dbcontext.rows.Any();
 
-        user.Id = model.Id;
-        user.Fullname = model.Fullname;
-        user.CreatedAt = model.CreatedAt = DateTimeOffset.UtcNow.ToLocalTime();
-        user.Phone = model.Phone;
-
-        try
+        if(!first)
         {
-            _dbcontext.rows.Add(user);
-            _dbcontext.SaveChanges();
+            var user = new RowViewModel();
+            user.Id = model.Id;
+            user.Fullname = model.Fullname;
+            user.CreatedAt = model.CreatedAt = DateTimeOffset.UtcNow.ToLocalTime();
+            user.Phone = model.Phone;
+            user.IsActive = model.IsActive = true;
+            try
+            {
+                _dbcontext.rows.Add(user);
+                _dbcontext.SaveChanges();
+            }
+            catch(ArgumentNullException)
+            {
+                System.Console.WriteLine("Null Keldi");
+            }
+            return RedirectToAction("ShowRow",user);
         }
 
-        catch(ArgumentNullException)
+        else
         {
-            System.Console.WriteLine("Rejected");
+            var lastuser = _dbcontext.rows.OrderBy(p => p.CreatedAt).LastOrDefault();
+            var user=new RowViewModel();
+            user.Id = model.Id;
+            user.Fullname = model.Fullname;
+            user.CreatedAt = model.CreatedAt = DateTimeOffset.UtcNow.ToLocalTime();
+            user.Phone=model.Phone;
+            user.IsActive = model.IsActive = true;
+            try
+            {
+                _dbcontext.rows.Add(user);
+                _dbcontext.SaveChanges();
+            }
+            catch(ArgumentNullException)
+            {
+                System.Console.WriteLine("Null Keldi");
+            }
+            return RedirectToAction("ShowRow",user);
         }
-        return RedirectToAction("ShowRow",user);
+
     }
     
     [HttpGet]
